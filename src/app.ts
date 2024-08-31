f7369c55bd58e37da354ec11717bd4c4a426090e
Abhay Kumar <codedev.org@gmail.com>
2024-08-31 15:32:18 +0530

    setup express server

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+//Routes
+app.get("/", (req, res, next) => {
+  res.json({
+    message: "Welcome to elib apis",
+  });
+});
+
+export default app;
